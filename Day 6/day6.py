8a19a99fe4b0462a22f495f415e7c9560db4953b
Xenans <xenansgaming@gmail.com>
2022-12-14 14:23:40 -0800

    Day 6!

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,2 +1,17 @@
 file = open('./Day 6/input.txt', 'r')
 input = [line.strip() for line in file]
+
+packet = 0
+message = 0
+
+for line in input:
+  for i in range(len(line)):
+    # print(line[i:i+4])
+    chars = set(line[i:i+4])
+    chars2 = set(line[i:i+14])
+    if len(chars) >= 4 and not packet:
+      packet = i+4
+    if len(chars2) >= 14 and not message:
+      message = i+14
+print(packet)
+print(message)
